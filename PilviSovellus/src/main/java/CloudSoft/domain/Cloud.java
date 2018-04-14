14387259f0470ea020fb93cf7219a6d2d0f99d8d
Sini Tenhunen <sini.tenhunen@helsinki.fi>
2018-04-14 15:32:24 +0300

    lisätty uusi cloud-luokka domainiin.

---
+++ PilviSovellus/src/main/java/CloudSoft/domain/Cloud.java
@@ -0,0 +1,28 @@
+package CloudSoft.domain;
+
+
+/**
+ *
+ * Tämä luokka kuvaa yhtä pilveä ja sen ominaisuuksia.
+ */
+
+public class Cloud {
+    
+    private boolean sataa;
+    private boolean iso;
+    private boolean valkoinenPilvi;
+    private boolean lapikuultava;
+    private boolean selvaRajainen;
+    private String ulkoNako;  // miten kysytään viimeinen asia.. Haetaanko tietokannasta ulko
+                                //näkötekstit ja käyttäjä valitsee vai...
+    
+    public Cloud() {
+        
+    }
+    
+    public void sataakoPilvi() {
+        this.sataa = true;
+    }
+    
+    public void 
+}
