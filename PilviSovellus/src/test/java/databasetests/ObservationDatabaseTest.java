bdd0c5dea14a60033c38195f0cae13b6357b25cd
Sini Tenhunen <sini.tenhunen@helsinki.fi>
2018-04-16 14:07:13 +0300

    Aikaisemmat testipaketit ja testi poistettu. Uudet testipaketit lisätty. Uudet (vielä tyhjät) testiluokat lisätty.

---
+++ PilviSovellus/src/test/java/databasetests/ObservationDatabaseTest.java
@@ -0,0 +1,41 @@
+package databasetests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * Tämä luokka testaa havaintotietokantaluokan metodeja.
+ */
+
+public class ObservationDatabaseTest {
+    
+    public ObservationDatabaseTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
---
+++ PilviSovellus/src/test/java/servicetest/CloudSoftServiceTest.java
@@ -0,0 +1,40 @@
+package servicetest;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * Tämä testiluokka vastaa CloudSoftService-luokan testaamisesta. 
+ */
+public class CloudSoftServiceTest {
+    
+    public CloudSoftServiceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
